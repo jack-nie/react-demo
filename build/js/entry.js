0d18234f25c15be01a3ff9ea77a242dd9a5e5f3c
jack-nie <hust.ud.hui@gmail.com>
2015-10-25 22:36:18 +0800

    add ajax support

--- build/js/entry.js
+++ build/js/entry.js
@@ -1,10 +1,7 @@
 var React    = require('react');
 var ReactDom = require('react-dom');
+var $        = require('jquery');
 
-var data = [
-  {author: "Pete Hunt", text: "This is one comment"}, 
-  {author: "Jordan Walke", text: "This is *another* comment"}
-];
 
 var CommentList = React.createClass({
   render: function() {
@@ -31,24 +28,46 @@ var Comment = React.createClass({
         <h2 className="commentAuthor">
          {this.props.author}
         </h2>
-        {this.props.children}
+         {this.props.children}
       </div>
     );
   }
 });
 
 var CommentBox = React.createClass({
+  loadCommentsFromServer: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err,toString());
+      }.bind(this)
+    });
+  },
+
+  getInitialState: function() {
+    return {data: []};
+  },
+
+  componentDidMount: function() {
+    this.loadCommentsFromServer();
+    setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+  },
+
   render: function() {
     return (
       <div className="commentBox">
         <h1>Comments</h1>
-        <CommentList data={this.props.data}/>
+        <CommentList data={this.state.data}/>
       </div>
     );
   }
 });
 
 ReactDom.render(
-  <CommentBox data={data}/>,
+  <CommentBox url="comments.json" pollInterval={2000}/>,
   document.getElementById('content')
 )
